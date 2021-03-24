9b6eaa66f2b20f209411e21981d2df131e1cbcaf
Haejin Green <atom82@hotmail.com>
2021-03-24 13:21:52 -0700

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./Banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNeflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}
+            )`,
+      }}
+    >
+      {" "}
+      {/*<<< Background image */}
+      <div className="banner_contents">
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_discription">{movie?.overview}</h1>
+      </div>
+      {/* title */}
+      {/*div > 2 buttons */}
+      {/* description */}
+    </header>
+  );
+}
+
+export default Banner;
